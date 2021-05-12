773890d8add05cb7169b60a8ba4cde664834cee2
soniyabijli <soniya@bijli-it-solutions.com>
2021-05-12 15:35:05 +0530

    trial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Hello team bijli !!!
+          Edit <code>src/App.js</code> Hello team bijli ,welcome to the journey!!!
         </p>
         <a
           className="App-link"
